61369852c3890ab6489c8ed23a0ed73eaa0c7347
ShruthiJakkala <146607676+ShruthiJakkala@users.noreply.github.com>
2024-04-21 14:08:43 -0400

    Update ViewRequestsJPanel.java

--- final/src/model/ViewRequestsJPanel.java
+++ final/src/model/ViewRequestsJPanel.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package UI.Requests;
 
 import model.EcoSystem;
@@ -17,10 +13,7 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumnModel;
 
-/**
- *
- * @author Suraj
- */
+
 public class ViewRequestsJPanel extends javax.swing.JPanel {
 
     /**
