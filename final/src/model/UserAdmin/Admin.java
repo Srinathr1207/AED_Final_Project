23887f03fc421e525181a1745bf61724f70dab52
ShruthiJakkala <146607676+ShruthiJakkala@users.noreply.github.com>
2024-04-21 14:01:47 -0400

    Update Admin.java

--- final/src/model/UserAdmin/Admin.java
+++ final/src/model/UserAdmin/Admin.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model.UserAdmin;
 
 import java.util.ArrayList;
 import model.Requests.Request;
 
-/**
- *
- * @author HP
- */
+
 public class Admin {
     private String fullName;
     private String userName;
