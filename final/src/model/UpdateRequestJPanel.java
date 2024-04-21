8bcd0ccc136d2c32ea20ddf442fa945aa5075d89
ShruthiJakkala <146607676+ShruthiJakkala@users.noreply.github.com>
2024-04-21 14:08:26 -0400

    Update UpdateRequestJPanel.java

--- final/src/model/UpdateRequestJPanel.java
+++ final/src/model/UpdateRequestJPanel.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package UI.Requests;
 
 import model.Areas.Area;
@@ -20,10 +16,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
-/**
- *
- * @author Suraj
- */
 public class UpdateRequestJPanel extends javax.swing.JPanel {
 
     /**
